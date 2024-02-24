def9903e87ed6033a07885914afdc7098bfde401
Dheeraj <86339497+DheerajPathrod@users.noreply.github.com>
2024-02-24 10:52:19 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# This is a YouTube Clone Application in React JS with Material UI 5
+
+1. This is a Basically a YouTube Clone.
+2. This built using HTML, CSS, JavaScript, ReactJS and Material UI.
+3. The RapidAPI is used to fetch all videos from all the categories mentioned in the website.
+
+
+
+![Screenshot (1561)](https://github.com/DheerajPathrod/Video-StreamingPlatform/assets/86339497/0359a880-5b17-4022-bc11-11cbbc8ec38a)
+
+
+![Screenshot (1562)](https://github.com/DheerajPathrod/Video-StreamingPlatform/assets/86339497/99e66c03-6f1f-4f81-9a6c-24ccd4f7a5aa)
+
+
+![Screenshot (1563)](https://github.com/DheerajPathrod/Video-StreamingPlatform/assets/86339497/6e12da0b-7f47-4610-9cda-75c38c3899f9)
---
+++ src/utils/constants.js
@@ -0,0 +1,41 @@
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+
+// export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+// export const logo = "https://logodix.com/logo/1062828.png";
+export const logo =
+  "https://cdn1.iconfinder.com/data/icons/youtuber/256/video-logo-play-icon-512.png";
+
+export const categories = [
+  { name: "New", icon: <HomeIcon /> },
+  { name: "Coding", icon: <CodeIcon /> },
+  { name: "Education", icon: <SchoolIcon /> },
+  { name: "Live", icon: <LiveTvIcon /> },
+  { name: "Music", icon: <MusicNoteIcon /> },
+  { name: "Gaming", icon: <SportsEsportsIcon /> },
+  { name: "Sport", icon: <FitnessCenterIcon /> },
+  { name: "Fashion", icon: <CheckroomIcon /> },
+  { name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
+  { name: "Comedy", icon: <TheaterComedyIcon /> },
+  { name: "Podcast", icon: <GraphicEqIcon /> },
+  { name: "Movie", icon: <OndemandVideoIcon /> },
+  { name: "Gym", icon: <FitnessCenterIcon /> },
+];
+
+export const demoThumbnailUrl = "";
+export const demoChannelUrl = "";
+export const demoVideoUrl = "";
+export const demoChannelTitle = "";
+export const demoVideoTitle = "";
+export const demoProfilePicture = "";
